9cee10a746de587232d965efe06b5256c42ba4bd
quasilyte <quasilyte@gmail.com>
2015-11-10 12:48:06 +0300

    bytechar trait

--- src/base/byte.rs
+++ src/base/byte.rs
@@ -3,6 +3,21 @@ pub type Bytes = [u8];
 
 pub type BytePredicate = fn(Byte) -> bool;
 
+// #TODO: maybe I need a new type rather than a typedef for `u8`?
+pub trait ByteChar {
+    fn is_digit(self) -> bool;
+}
+
+impl ByteChar for Byte {
+    // #TODO: check whenever `if` and ascii number range check is more efficient
+    fn is_digit(self) -> bool {
+        match self {
+            b'0'...b'9' => true,
+            _ => false
+        }
+    }
+}
+
 // #FIXME: sometimes works not how intended
 macro_rules! byte_predicate {
     ($($body: pat),*) => {{
