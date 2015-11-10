7f3bd28e840f20fe56cbd0da115e2396b5f3490c
quasilyte <quasilyte@gmail.com>
2015-11-10 10:48:03 +0300

    new error message

--- src/env/error.rs
+++ src/env/error.rs
@@ -1,6 +1,8 @@
 use env::Announce;
 use base::Bytes;
 
+// #FIXME: need a better error reporting API
+
 // #TODO: write helper macro which takes N expressions,
 // builds an Announce and sends it as error
 
@@ -9,8 +11,16 @@ pub fn decimal_parse() -> ! {
         .send_as_error(1);
 }
 
-pub fn unexpected_token(unit: &Bytes) -> ! {
+pub fn unexpected_token(bytes: &Bytes) -> ! {
     Announce::new("unexpected token met")
-        .add_details(&format!("looking at `{}`", String::from_utf8(unit.to_owned()).unwrap()))
+        .add_details(&format!("looking at `{}`", String::from_utf8(bytes.to_owned()).unwrap()))
+        .add_context("on tokenization")
+        .send_as_error(2);
+}
+
+pub fn malformed_identifier(bytes: &Bytes) -> ! {
+    Announce::new("found invalid character sequence")
+        .add_details(&format!("looking at `{}`", String::from_utf8(bytes.to_owned()).unwrap()))
+        .add_context("on tokenization; parsing identifier")
         .send_as_error(2);
 }
