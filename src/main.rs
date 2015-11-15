81a1ab7c76857113aadcbf99f4cfde3cd1f6a9ea
quasilyte <quasilyte@gmail.com>
2015-11-16 01:21:26 +0300

    removing some commented-out code from main

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,6 @@ struct SchemeParser<'a> {
     tokens: LexerIter<'a>
 }
 
-/*
-fn fn_call(name: Box<Node>, mut args: Vec<Box<Node>>) -> Box<Node> {
-    match args.len() {
-        1 => Box::new(UnaryPlus::new(args.pop().unwrap())),
-        _ => Box::new(Plus::new(args))
-    }
-}
-*/
-
 impl<'a> SchemeParser<'a> {
     fn new(input: &'a Bytes) -> Self {
         let mut lexer = Lexer::new();    
@@ -64,7 +55,6 @@ impl<'a> SchemeParser<'a> {
         }
         
         match self.tokens.next().unwrap() {
-            // Token::Ident(name) => fn_call(name, collect_args!()),
             Token::Plus => {
                 let mut args = collect_args!();
                 match args.len() {
@@ -77,23 +67,11 @@ impl<'a> SchemeParser<'a> {
     }
     
     fn parse(&mut self, token: Token) -> Box<Node> {
-        // use base::Token::*;
-
         match token {
             Token::LeftParen => self.fn_call(),
             Token::Decimal(x) => Box::new(x),
             _ => panic!("unexpected {:?}", token)
         }
-        
-        /*
-        match token {
-            LeftParen => println!("lparen"),
-            Ident(k) => println!("keyword! {:?}", k),
-            Whitespace | Tab | Newline => (),
-            LineComment(text) => println!("comment with text {:?}", text),
-            _ => println!("{:?}", token)
-        }
-        */
     }
 }
 
@@ -104,8 +82,6 @@ fn main() {
     // #TODO: lexer must ensure trailing delimiter char in the input,
     // because we do not want to make excessive checks at run time
     // let input = b"(define x (+ 10 20.4)) ";
-    // let input = br##" (define str "string content!") "##;
-    // let input = br##""s\"tr" "##;
     // let input = include_bytes!("../tmp/input.txt");
     // let input = b"#comment\n(+ 10 20) ";
     let input = b"(+ 1 2)(+ 4 5 5) ";
