e59f941c83355c4939b0707bf6b52f5a17042ad6
quasilyte <quasilyte@gmail.com>
2015-11-29 22:29:04 +0300

    inlined some arr functions

--- src/containers/arr/headers/arr_decl_gen.h
+++ src/containers/arr/headers/arr_decl_gen.h
@@ -2,25 +2,57 @@
 
 #include <stddef.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <assert.h>
 
-#define ARR_DECL(ID, PREFIX, ELT_T)			\
-  typedef struct ID ID;					\
-  struct ID {						\
-    size_t cap;						\
-    ELT_T* mem;						\
-  };							\
-							\
-  ID PREFIX##_new(size_t cap);				\
-  void PREFIX##_init(ID* self, size_t cap);		\
-  void PREFIX##_realloc(ID* self, size_t cap);		\
-  void PREFIX##_enlarge(ID* self);			\
-  void PREFIX##_ensure_cap(ID* self, size_t cap);	\
-  void PREFIX##_free(ID* self);				\
-							\
-  static inline bool PREFIX##_is_null(const ID* self);	\
-							\
-  /* Inline functions implementation */			\
-							\
-  static inline bool PREFIX##_is_null(const ID* self) {	\
-    return NULL == self->mem;				\
-  } 
+#define ARR_DECL(ID, PREFIX, ELT_T)					\
+  typedef struct ID ID;							\
+  struct ID {								\
+    size_t cap;								\
+    ELT_T* mem;								\
+  };									\
+									\
+  static inline ID PREFIX##_new(size_t cap);				\
+  static inline void PREFIX##_init(ID* self, size_t cap);		\
+  void PREFIX##_realloc(ID* self, size_t cap);				\
+  void PREFIX##_enlarge(ID* self);					\
+  static inline void PREFIX##_free(ID* self);				\
+									\
+  static inline bool PREFIX##_is_null(const ID* self);			\
+									\
+  static inline void PREFIX##_ensure_cap(ID* self, size_t cap);		\
+									\
+  /* Inline functions implementation */					\
+									\
+  static inline ID PREFIX##_new(size_t cap) {				\
+    ELT_T* mem = malloc(cap * sizeof(ELT_T));				\
+    assert(mem != NULL);						\
+    return (ID) {							\
+      .cap = cap,							\
+      .mem = mem							\
+    };  								\
+  }									\
+									\
+  static inline void PREFIX##_init(ID* self, size_t cap) {		\
+    ELT_T* mem = malloc(cap * sizeof(ELT_T));				\
+    assert(NULL != mem);						\
+    self->mem = mem;							\
+    self->cap = cap;							\
+  }									\
+									\
+  static inline void PREFIX##_free(ID* self) {				\
+    assert(self->mem != NULL);						\
+    free(self->mem);							\
+    self->mem = NULL;							\
+  }									\
+									\
+  static inline void PREFIX##_ensure_cap(ID *self, size_t cap) {	\
+    if (self->cap < cap ) {						\
+      PREFIX##_realloc(self, cap + (cap >> 1));				\
+    }									\
+  }									\
+									\
+  static inline bool PREFIX##_is_null(const ID* self) {			\
+    return NULL == self->mem;						\
+  }									\
+
--- src/containers/arr/headers/arr_impl_gen.h
+++ src/containers/arr/headers/arr_impl_gen.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <stdlib.h>
-#include <assert.h>
-
 /**
  * Default implementation generators for trivial cases
  * like array of chars (char*) or array of ints (int*).
@@ -11,30 +8,12 @@
  * So, user defined type like Point is OK too
  */
 
-#define ARR_NEW(ID, PREFIX, ELT_T)			\
-  ID PREFIX##_new(size_t cap) {				\
-    ELT_T* mem = malloc(cap * sizeof(ELT_T));		\
-    assert(mem != NULL);				\
-    return (ID) {					\
-      .cap = cap,					\
-      .mem = mem					\
-    };   						\
-  }
-
-#define ARR_INIT(ID, PREFIX, ELT_T)		\
-  void PREFIX##_init(ID* self, size_t cap) {	\
-    ELT_T* mem = malloc(cap * sizeof(ELT_T));	\
-    assert(NULL != mem);			\
-    self->mem = mem;				\
-    self->cap = cap;				\
-  }    
-
-#define ARR_REALLOC(ID, PREFIX, ELT_T)				      \
-  void PREFIX##_realloc(ID* self, size_t cap) {			      \
-    ELT_T* new_mem = realloc(self->mem, cap * sizeof(ELT_T));	      \
-    assert(new_mem != NULL);					      \
-    self->mem = new_mem;					      \
-    self->cap = cap;						      \
+#define ARR_REALLOC(ID, PREFIX, ELT_T)				\
+  void PREFIX##_realloc(ID* self, size_t cap) {			\
+    ELT_T* new_mem = realloc(self->mem, cap * sizeof(ELT_T));	\
+    assert(new_mem != NULL);					\
+    self->mem = new_mem;					\
+    self->cap = cap;						\
   }
 
 #define ARR_ENLARGE(ID, PREFIX, ELT_T)					\
@@ -45,24 +24,6 @@
     self->mem = new_mem;						\
   }
 
-#define ARR_ENSURE_CAP(ID, PREFIX)		   \
-  void PREFIX##_ensure_cap(ID *self, size_t cap) { \
-    if (self->cap < cap ) {			   \
-      PREFIX##_realloc(self, cap + (cap >> 1));	   \
-    }						   \
-  }
-
-#define ARR_FREE(ID, PREFIX, ELT_T)			\
-  void PREFIX##_free(ID* self) {			\
-    assert(self->mem != NULL);				\
-    free(self->mem);					\
-    self->mem = NULL;					\
-  }
-
 #define ARR_IMPL(ID, PREFIX, ELT_T)		\
-  ARR_NEW(ID, PREFIX, ELT_T)			\
-  ARR_INIT(ID, PREFIX, ELT_T)			\
   ARR_REALLOC(ID, PREFIX, ELT_T)		\
-  ARR_ENLARGE(ID, PREFIX, ELT_T)		\
-  ARR_ENSURE_CAP(ID, PREFIX)			\
-  ARR_FREE(ID, PREFIX, ELT_T)
+  ARR_ENLARGE(ID, PREFIX, ELT_T)		
