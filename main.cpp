4a0786267cda0c0dd0d6630da88b7d5f0bb02bb3
quasilyte <quasilyte@gmail.com>
2016-01-28 11:24:52 +0300

    simple sum type sketch

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "runtime/core/types.hpp"
+#include "runtime/opt/sum_type.hpp"
 #include "lexer/token.hpp"
 #include "lexer/char_type.hpp"
 #include "lexer/token_stream.hpp"
@@ -12,6 +13,7 @@
 int main() {
   // auto input = slurp("../Resembler/input/lua/arith.lua");
 
+  /*
   std::string input{"14 (x y {z 4.6}) [bar()]"};
   printf("Input:\n`%s`\n", input.c_str());
 
@@ -20,6 +22,11 @@ int main() {
   while (tokens.next_tag() != SOURCE_END) {
     dump_token(tokens.current());
   }
+  */
+
+  Any a(10L);
+  Any b(5.5);
+  printf("%f\n", add(b, a).get_real());
 
   return 0;
 }
