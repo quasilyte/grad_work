40760d39fb44d530f2371fb94e207d0a5cb473de
quasilyte <quasilyte@gmail.com>
2016-05-07 11:30:35 +0300

    added another hashing function

---
+++ src/djb2_hash.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "typedefs.hpp"
+
+namespace djb2_hash {
+  inline u64 encode(const char* cstr);
+  inline u64 encode(const char* bytes, u32 length);
+}
+
+inline u64 djb2_hash::encode(const char* cstr) {
+  usize hash = static_cast<usize>(5381);
+
+  for (auto p = cstr; *p; ++p) {
+    hash = ((hash << 5) + hash) + *p;
+  }
+
+  return hash;
+}
+
+inline u64 djb2_hash::encode(const char* bytes, u32 count) {
+  u64 hash = static_cast<u64>(5381);
+
+  for (uint i = 0; i < count; ++i) {
+    hash = ((hash << 5) + hash) + bytes[i];
+  }
+
+  return hash;
+}
