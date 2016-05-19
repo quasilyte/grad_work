dcee4b4b76c14e95cbacaebb56d715c96167815a
quasilyte <quasilyte@gmail.com>
2016-05-19 18:26:06 +0300

    moved lists impl into headers

--- src/dt/alist.hpp
+++ src/dt/alist.hpp
@@ -15,18 +15,38 @@ public:
     T val;
 
     Node() = default;
-    Node(const StrView& key, T val);
+    Node(const StrView& key, T val): key{key}, val{val} {}
   };
 
-  void Insert(const StrView& key, T val);
+  void Insert(const StrView& key, T val) {
+    nodes.push_back(Node{key, val});
+  }
 
-  void Drop(int n);
+  void Drop(int n) {
+    nodes.resize(nodes.size() - n);
+  }
 
-  T Find(const StrView& key, int limit) const noexcept;
-  T Find(const StrView &key) const noexcept;
+  T Find(const StrView& key, int limit) const noexcept {
+    auto end = nodes.rend() - (Size() - limit);
+    for (auto it = nodes.rbegin(); it != end; ++it) {
+      if (it->key == key) {
+        return it->val;
+      }
+    }
 
-  uint Size() const noexcept;
+    return T{};
+  }
+
+
+  T Find(const StrView &key) const noexcept {
+    return Find(key, Size());
+  }
+
+  uint Size() const noexcept {
+    return nodes.size();
+  }
 
 private:
   std::vector<Node> nodes;
 };
+
