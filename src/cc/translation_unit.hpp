1f8b8bc6eaa070596ceacc7535d15e212566a6fb
quasilyte <quasilyte@gmail.com>
2016-05-21 22:34:11 +0300

    T.U. now carries lambdas

--- src/cc/translation_unit.hpp
+++ src/cc/translation_unit.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "sym/module.hpp"
-#include "sym/lambda.hpp"
 #include <vector>
 
 namespace ast {
   struct Node;
   struct DefVar;
   struct DefFunc;
+}
+
+namespace sym {
+  struct Lambda;
 }
 
 namespace cc {
@@ -18,7 +21,7 @@ struct cc::TranslationUnit {
   std::vector<dt::StrView> structs;
   std::vector<ast::DefVar*> globals;
   std::vector<ast::Node*> exprs;
-  std::vector<sym::Lambda> lambdas;
+  std::vector<sym::Lambda*> lambdas;
 
   sym::Module module;
 };
