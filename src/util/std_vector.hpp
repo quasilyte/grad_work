c2ee650b9a80fc74e216d47ccdf65295631a22d8
quasilyte <quasilyte@gmail.com>
2016-05-22 21:42:29 +0300

    util function (because std::algorithm designed for aliens)

---
+++ src/util/std_vector.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+namespace util {
+  template<class IN, class OUT>
+  std::vector<OUT> map(std::vector<IN> elems, OUT(*fn)(IN));
+}
+
+template<class IN, class OUT>
+std::vector<OUT> util::map(std::vector<IN> elems, OUT(*fn)(IN)) {
+  std::vector<OUT> result;
+  result.reserve(elems.size());
+
+  for (IN elem : elems) {
+    result.push_back(fn(elem));
+  }
+
+  return result;
+}
