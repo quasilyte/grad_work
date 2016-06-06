7f489244319b0db150702a5bce28c9d191d7be3b
quasilyte <quasilyte@gmail.com>
2016-06-06 23:59:36 +0300

    more typedefs

---
+++ src/sym/attr.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "sym/param.hpp"
+
+namespace sym {
+  typedef Param Attr; // This can change in future
+}
---
+++ src/sym/typedefs.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <deps/cxx/vector.hpp>
+
+namespace sym {
+  struct Param;
+  struct Attr;
+
+  typedef std::vector<Param> ParamList;
+  typedef std::vector<Attr> AttrList;
+}
